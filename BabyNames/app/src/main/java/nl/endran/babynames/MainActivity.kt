ae42eea9a9abff270d7b5b1ad078f410cb3c3ffb
David Hardy <david.hardy@philips.com>
2015-12-14 19:12:10 +0100

    Converted to Kotlin

---
+++ BabyNames/app/src/main/java/nl/endran/babynames/MainActivity.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2015 by David Hardy. Licensed under the Apache License, Version 2.0.
+ */
+
+package nl.endran.babynames
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        fab.setOnClickListener { Snackbar.make(it, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
